b61de9143eac2f532847c4bdb8b83e4a215484d5
Denzel Jones <denzeljones442@gmail.com>
2024-07-29 14:00:50 +0300

    Modify directory handling

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
@@ -8,17 +11,14 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Step 1: Make sure that when a user visits the home page,
-//   it shows a random activity.You will need to check the format of the
-//   JSON data from response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
-    res.render("index.ejs", { data: result });
+    res.render(__dirname + "/views/index.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
+    res.render(__dirname + "/views/index.ejs", {
       error: error.message,
     });
   }
@@ -34,23 +34,14 @@ app.post("/", async (req, res) => {
     const result = response.data;
     console.log(result);
     var random = Math.floor(Math.random() * result.length);
-    res.render("index.ejs", { data: result[random] });
+    res.render(__dirname + "/views/index.ejs", { data: result[random] });
   } catch (error) {
     console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
+    res.render(__dirname + "/views/index.ejs", {
       error: "No activities that match your criteria",
     });
   }
 
-  // res.render("index.ejs", {activity : result})
-  // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
-  // Render the index.ejs file with a single *random* activity that comes back
-  // from the API request.
-  // Step 3: If you get a 404 error (resource not found) from the API request.
-  // Pass an error to the index.ejs to tell the user:
-  // "No activities that match your criteria."
 });
 
 app.listen(port, () => {
